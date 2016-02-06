7fc27383153e27cd92c2e708b6e113e48e0b4932
Heitor Augusto Pereira <HeitorPereira@macbook-pro-de-heitor.local>
2016-02-06 20:05:12 -0200

    Initial Commit

---
+++ RadioGospel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadioGospel
+//
+//  Created by Heitor Augusto Pereira on 06/02/16.
+//  Copyright © 2016 Heitor Augusto Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
